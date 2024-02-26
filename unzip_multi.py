3025d067c58265a7e3e100f10bf870856d56a092
Yewon <ga06033@yonsei.ac.kr>
2024-02-26 15:32:26 +0000

    feat: added hdtf

---
+++ unzip_multi.py
@@ -0,0 +1,48 @@
+# unzip zip files using multi-thread and Multiprocessing. Note this op is not thread-safe.
+
+import zipfile
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+import argparse 
+from glob import glob 
+import os.path as osp
+from tqdm import tqdm
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Unzip zip files using multi-thread")
+    parser.add_argument('--data_root', type=str, default='path/to/your/*.zip', help='path to the zip files')
+    parser.add_argument('--extract_path', type=str, default='path/to/extract', help='path to extract the zip files')
+    parser.add_argument('--num_threads', type=int, default=4, help='number of threads to use')
+    parser.add_argument('--num_processes', type=int, default=16, help='number of processes to use')
+    args = parser.parse_args()
+    return args
+
+def unzip_file(zip_file, extract_path):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extract_path)
+
+
+def multi_thread_unzip(zip_file, extract_path, num_threads):
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        # Submit tasks to the executor
+        futures = [executor.submit(unzip_file, zip_file, extract_path) for _ in range(num_threads)]
+
+        # Wait for all tasks to complete
+        for future in futures:
+            future.result()
+
+def multi_thread_and_process_unzip(zip_files, extract_path, num_threads, num_processes):
+    with ProcessPoolExecutor(max_workers=num_processes) as process_executor:
+        # Multiprocessing: Submit tasks to the process executor
+        process_futures = [process_executor.submit(multi_thread_unzip, zip_file, extract_path, num_threads) for zip_file in zip_files]
+
+        # Wait for all multiprocessing tasks to complete
+        for future in tqdm(process_futures, total=len(process_futures)):
+            future.result()
+
+def main():
+    args = parse_args()
+    zip_files = glob(osp.join(args.data_root, '*.zip'))
+    multi_thread_and_process_unzip(zip_files, args.extract_path, args.num_threads, args.num_processes)
+
+if __name__ == '__main__':
+    main()
--- voxcelb-from-hgface.sh
+++ voxcelb-from-hgface.sh
@@ -1,10 +1,13 @@
 # !/bin/bash
 
-# for i in 1 2 3 4 5 6
-# do
-#     # wget -O /home/ubuntu/datasets/VoxCeleb/vox2_mp4_$i.zip https://huggingface.co/datasets/ProgramComputer/voxceleb/resolve/main/vox2/vox2_mp4_$i.zip 
-#     aria2c -s16 -x16 -d $1 -o vox2_mp4_$i.zip https://huggingface.co/datasets/ProgramComputer/voxceleb/resolve/main/vox2/vox2_mp4_$i.zip 
-# done
+aria2c -s16 -x16 -d $1 -o vox1_dev_wav.zip https://huggingface.co/datasets/ProgramComputer/voxceleb/resolve/main/vox1/vox1_dev_wav.zip
+aria2c -s16 -x16 -d $1 -o vox1_test_wav.zip https://huggingface.co/datasets/ProgramComputer/voxceleb/resolve/main/vox1/vox1_test_wav.zip
+
+for i in 1 2 3 4 5 6
+do
+    # wget -O /home/ubuntu/datasets/VoxCeleb/vox2_mp4_$i.zip https://huggingface.co/datasets/ProgramComputer/voxceleb/resolve/main/vox2/vox2_mp4_$i.zip 
+    aria2c -s16 -x16 -d $1 -o vox2_mp4_$i.zip https://huggingface.co/datasets/ProgramComputer/voxceleb/resolve/main/vox2/vox2_mp4_$i.zip 
+done
 
 for i in 1 2
 do 
